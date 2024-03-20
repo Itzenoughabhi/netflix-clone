81e4db4a00a497eb2ad6d385af187228a00104b1
ABHISHEK YADAV <151519170+Itzenoughabhi@users.noreply.github.com>
2024-03-20 08:33:35 +0530

    Update script.js

--- netflix/script.js
+++ netflix/script.js
@@ -1,15 +1,8 @@
- alert("hello") 
- function toggleAnswer(question) {
-    const answer = question.nextElementSibling;
-    const allAnswers = document.querySelectorAll('.answer');
-    
-    // Close all answers
-    allAnswers.forEach(function(ans) {
-        if (ans.style.display === 'block' && ans.previousElementSibling !== question) {
-            ans.style.display = 'none';
-        }
-    });
-
-    // Toggle the current answer
-    answer.style.display = answer.style.display === 'none' ? 'block' : 'none';
+function toggleAnswer(question) {
+    var answer = question.nextElementSibling;
+    if (answer.style.display === 'block') {
+        answer.style.display = 'none';
+    } else {
+        answer.style.display = 'block';
+    }
 }
